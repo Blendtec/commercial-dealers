749c7269239f456be021b2a31fc7031565a7cc44
Reid Kuttler <reidkuttler@gmail.com>
2018-08-13 12:25:47 -0700

    feat(rewrote in react): rewrote app in react

---
+++ src/enum/Commercial.enum.js
@@ -0,0 +1,11 @@
+const Commercial = Object.freeze({
+    RESIDENTIALURL: 'dealers/residential/',
+    INTERNATIONALURL: 'dealers/international/',
+    USA: 'United States',
+    INTERNATIONAL: 'International',
+    STATESURL: '/states.json',
+    COUNTRIESURL: '/countries.json',
+    DEFAULT: 'default',
+});
+
+export default Commercial;
